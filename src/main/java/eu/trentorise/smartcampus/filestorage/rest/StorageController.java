4f849b935d0d978f1f86ad39874eb93771471e56
mirkoperillo <mirko.perillo@gmail.com>
2013-07-24 16:59:09 +0200

    Added oauth security to Storage

--- src/main/java/eu/trentorise/smartcampus/filestorage/rest/StorageController.java
+++ src/main/java/eu/trentorise/smartcampus/filestorage/rest/StorageController.java
@@ -19,6 +19,7 @@ import eu.trentorise.smartcampus.filestorage.model.NotFoundException;
 import eu.trentorise.smartcampus.filestorage.model.SmartcampusException;
 import eu.trentorise.smartcampus.filestorage.model.Storage;
 import eu.trentorise.smartcampus.resourceprovider.controller.SCController;
+import eu.trentorise.smartcampus.resourceprovider.model.AuthServices;
 
 @Controller
 public class StorageController extends SCController {
@@ -32,72 +33,70 @@ public class StorageController extends SCController {
 	@Autowired
 	private PermissionManager permissionManager;
 
-	@RequestMapping(method = RequestMethod.POST, value = "/storage")
+	@Autowired
+	AuthServices authServices;
+
+	@RequestMapping(method = RequestMethod.POST, value = "/storage/{appName}")
 	public @ResponseBody
-	Storage create(HttpServletRequest request, @RequestBody Storage storage)
-			throws SmartcampusException, AlreadyStoredException {
-		String appId = retrieveAppId(request);
-		storage.setAppName(appId);
+	Storage create(HttpServletRequest request, @RequestBody Storage storage,
+			@PathVariable String appName) throws SmartcampusException,
+			AlreadyStoredException {
+		storage.setAppName(appName);
 		return storageManager.save(storage);
 	}
 
-	@RequestMapping(method = RequestMethod.PUT, value = "/storage/{storageId}")
+	@RequestMapping(method = RequestMethod.PUT, value = "/storage/{appName}/{storageId}")
 	public @ResponseBody
 	Storage update(HttpServletRequest request, @RequestBody Storage storage,
-			@PathVariable String storageId) throws SmartcampusException,
-			NotFoundException {
+			@PathVariable String storageId, @PathVariable String appName)
+			throws SmartcampusException, NotFoundException {
 
 		storage.setId(storageId);
 
-		// TODO retrieved from SecurityContext
-		String appId = retrieveAppId(request);
-		if (!permissionManager.checkStoragePermission(appId, storageId)) {
+		if (!permissionManager.checkStoragePermission(appName, storageId)) {
 			throw new SecurityException();
 		}
 		return storageManager.update(storage);
 	}
 
-	@RequestMapping(method = RequestMethod.DELETE, value = "/storage/{storageId}")
+	@RequestMapping(method = RequestMethod.DELETE, value = "/storage/{appName}/{storageId}")
 	public @ResponseBody
-	boolean delete(HttpServletRequest request, @PathVariable String storageId)
-			throws SmartcampusException {
+	boolean delete(HttpServletRequest request, @PathVariable String storageId,
+			@PathVariable String appName) throws SmartcampusException {
 
-		// TODO retrieved from SecurityContext
-		String appId = retrieveAppId(request);
-		if (!permissionManager.checkStoragePermission(appId, storageId)) {
+		if (!permissionManager.checkStoragePermission(appName, storageId)) {
 			throw new SecurityException();
 		}
 		storageManager.delete(storageId);
 		return true;
 	}
 
-	@RequestMapping(method = RequestMethod.GET, value = "/storage")
+	@RequestMapping(method = RequestMethod.GET, value = "/storage/{appName}")
 	public @ResponseBody
-	ListStorage getStorages(HttpServletRequest request)
-			throws SmartcampusException {
-
-		// TODO retrieved from SecurityContext
-		String appId = retrieveAppId(request);
+	ListStorage getStorages(HttpServletRequest request,
+			@PathVariable String appName) throws SmartcampusException {
 
 		ListStorage result = new ListStorage();
-		result.setStorages(storageManager.getStorages(appId));
+		result.setStorages(storageManager.getStorages(appName));
 		return result;
 	}
 
-	@RequestMapping(method = RequestMethod.GET, value = "/storage/{storageAccountId}")
+	@RequestMapping(method = RequestMethod.GET, value = "/storage/{appName}/{storageId}")
 	public @ResponseBody
 	Storage getStorage(HttpServletRequest request,
-			@PathVariable String storageAccountId) throws SmartcampusException,
-			NotFoundException {
+			@PathVariable String storageId, @PathVariable String appName)
+			throws SmartcampusException, NotFoundException {
 
-		// TODO retrieved from SecurityContext
-		String appId = retrieveAppId(request);
-
-		if (!permissionManager.checkStoragePermission(appId, storageAccountId)) {
+		if (!permissionManager.checkStoragePermission(appName, storageId)) {
 			throw new SecurityException();
 		}
 
-		return storageManager.getStorageById(storageAccountId);
+		return storageManager.getStorageById(storageId);
+	}
+
+	@Override
+	protected AuthServices getAuthServices() {
+		return authServices;
 	}
 
 }
