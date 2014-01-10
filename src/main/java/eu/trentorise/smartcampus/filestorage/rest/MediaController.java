d3bf2b991eb3d9203d9c611e62fb57841aaab87a
mirkoperillo <mirko.perillo@gmail.com>
2014-01-10 14:49:41 +0100

    changed GET metadata security check

--- src/main/java/eu/trentorise/smartcampus/filestorage/rest/MediaController.java
+++ src/main/java/eu/trentorise/smartcampus/filestorage/rest/MediaController.java
@@ -217,7 +217,7 @@ public class MediaController extends SCController {
 			SecurityException, NotFoundException {
 		User user = getUserObject(getUserId());
 
-		if (!permissionManager.checkResourcePermission(user, appId, resourceId)) {
+		if (!permissionManager.checkSharingPermission(user, resourceId)) {
 			throw new SecurityException();
 		}
 		return metadataManager.findByResource(resourceId);
